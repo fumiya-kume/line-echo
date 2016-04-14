e316babe1d0821d10fee9d3758c864657de6ba51
久米　史也 <fumiya.kume@hotmail.com>
2016-04-14 23:53:25 +0900

    Back Up

--- app.rb
+++ app.rb
@@ -10,11 +10,15 @@ class App < Sinatra::Base
 
     params['result'].each do |msg|
       request_content = {
-        to: [msg['content']['from']],
-        toChannel: 1383378250, # Fixed  value
-        eventType: "138311608800106203", # Fixed value
-        #content: msg['content']
-        content => {contentType:1,toType:1,text:[msg['content']['from']]}
+          :to => [msg['content']['from']],
+          :toChannel => 1383378250, # Fixed  value
+          :eventType => "138311608800106203", # Fixed value
+          content: msg['content']
+          #content => {
+          #    :contentType => 1,
+          #    :toType => 1,
+          #    :text => [msg['content']['from']]
+          #}
       }
 
       endpoint_uri = 'https://trialbot-api.line.me/v1/events'
@@ -22,10 +26,10 @@ class App < Sinatra::Base
 
       RestClient.proxy = ENV['FIXIE_URL'] if ENV['FIXIE_URL']
       RestClient.post(endpoint_uri, content_json, {
-        'Content-Type' => 'application/json; charset=UTF-8',
-        'X-Line-ChannelID' => ENV["LINE_CHANNEL_ID"],
-        'X-Line-ChannelSecret' => ENV["LINE_CHANNEL_SECRET"],
-        'X-Line-Trusted-User-With-ACL' => ENV["LINE_CHANNEL_MID"],
+          'Content-Type' => 'application/json; charset=UTF-8',
+          'X-Line-ChannelID' => ENV["LINE_CHANNEL_ID"],
+          'X-Line-ChannelSecret' => ENV["LINE_CHANNEL_SECRET"],
+          'X-Line-Trusted-User-With-ACL' => ENV["LINE_CHANNEL_MID"],
       })
     end
 
