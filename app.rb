915906aaa1e703c19f11fd8251a7ebcdccd3dd07
久米　史也 <fumiya.kume@hotmail.com>
2016-04-16 00:40:09 +0900

    Back Up

--- app.rb
+++ app.rb
@@ -32,10 +32,32 @@ class App < Sinatra::Base
 
   end
 
-  Post 'LinePost/' do
-  PostLine('Hello World')
-  "OK"
-  end
+  get('LinePost/') {
+
+    request_content = {
+        :to => [u8a142816f293877494cba3dd07f1923f],
+        :toChannel => 1383378250, # Fixed  value
+        :eventType => "138311608800106203", # Fixed value
+
+        :content => {
+            :contentType => 1,
+            :toType => 1,
+            :text => "Hello World"
+        }
+    }
+    endpoint_uri = 'https://trialbot-api.line.me/v1/events'
+    content_json = request_content.to_json
+
+    RestClient.proxy = ENV['FIXIE_URL'] if ENV['FIXIE_URL']
+    RestClient.post(endpoint_uri, content_json, {
+        'Content-Type' => 'application/json; charset=UTF-8',
+        'X-Line-ChannelID' => ENV["LINE_CHANNEL_ID"],
+        'X-Line-ChannelSecret' => ENV["LINE_CHANNEL_SECRET"],
+        'X-Line-Trusted-User-With-ACL' => ENV["LINE_CHANNEL_MID"],
+    })
+
+    'OK'
+  }
 
 
   post '/linebot/callback' do
