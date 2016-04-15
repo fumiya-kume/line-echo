d2e63ab97d4fc686d4638da8f73ef276bc162d42
久米　史也 <fumiya.kume@hotmail.com>
2016-04-16 00:28:31 +0900

    Back Up

--- app.rb
+++ app.rb
@@ -5,6 +5,38 @@ require 'json'
 require 'rest-client'
 
 class App < Sinatra::Base
+
+  # @param [string] 
+  def PostLine(message)
+    request_content = {
+        :to => [u8a142816f293877494cba3dd07f1923f],
+        :toChannel => 1383378250, # Fixed  value
+        :eventType => "138311608800106203", # Fixed value
+
+        :content => {
+            :contentType => 1,
+            :toType => 1,
+            :text => message
+        }
+    }
+    endpoint_uri = 'https://trialbot-api.line.me/v1/events'
+    content_json = request_content.to_json
+
+    RestClient.proxy = ENV['FIXIE_URL'] if ENV['FIXIE_URL']
+    RestClient.post(endpoint_uri, content_json, {
+        'Content-Type' => 'application/json; charset=UTF-8',
+        'X-Line-ChannelID' => ENV["LINE_CHANNEL_ID"],
+        'X-Line-ChannelSecret' => ENV["LINE_CHANNEL_SECRET"],
+        'X-Line-Trusted-User-With-ACL' => ENV["LINE_CHANNEL_MID"],
+    })
+
+  end
+
+  get 'LinePost/:message' do |m|
+    PostLine(m)
+  end
+
+
   post '/linebot/callback' do
     params = JSON.parse(request.body.read)
 
@@ -15,20 +47,7 @@ class App < Sinatra::Base
           :eventType => "138311608800106203", # Fixed value
           #オウム返しする場合
 
-
-          # :content => msg['content']
-
-          # :content => {
-          #     :location => null,
-          #     :id => "325708",
-          #     :contentType => 1,
-          #     :from => "uff2aec188e58752ee1fb0f9507c6529a",
-          #     :createdTime => 1332394961610,
-          #     :to => ["u0a556cffd4da0dd89c94fb36e36e1cdc"],
-          #     :toType => 1,
-          #     :contentMetadata => null,
-          #     :text => "Hello, BOT API Server!"
-          # }
+          #[u8a142816f293877494cba3dd07f1923f]
 
           content: {
               :contentType => 1,
@@ -51,4 +70,41 @@ class App < Sinatra::Base
 
     "OK"
   end
+
+  # post '/linebot/callback' do
+  #   params = JSON.parse(request.body.read)
+  #
+  #   params['result'].each do |msg|
+  #     request_content = {
+  #         :to => [msg['content']['from']],
+  #         :toChannel => 1383378250, # Fixed  value
+  #         :eventType => "138311608800106203", # Fixed value
+  #         #オウム返しする場合
+  #
+  #         #[u8a142816f293877494cba3dd07f1923f]
+  #
+  #         content: {
+  #             :contentType => 1,
+  #             :toType => 1,
+  #             :text => [msg['content']['from']]
+  #         }
+  #     }
+  #
+  #     endpoint_uri = 'https://trialbot-api.line.me/v1/events'
+  #     content_json = request_content.to_json
+  #
+  #     RestClient.proxy = ENV['FIXIE_URL'] if ENV['FIXIE_URL']
+  #     RestClient.post(endpoint_uri, content_json, {
+  #         'Content-Type' => 'application/json; charset=UTF-8',
+  #         'X-Line-ChannelID' => ENV["LINE_CHANNEL_ID"],
+  #         'X-Line-ChannelSecret' => ENV["LINE_CHANNEL_SECRET"],
+  #         'X-Line-Trusted-User-With-ACL' => ENV["LINE_CHANNEL_MID"],
+  #     })
+  #   end
+  #
+  #   "OK"
+  # end
+
+end
+
 end
