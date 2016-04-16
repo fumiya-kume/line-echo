803ad953b3c5f411d6f3b2f68cf92ec9be69085e
Ruby mine < 7dbedce569ac3038e978a9bc9b89313a1aee5edd>
2016-04-16 12:52:58 +0900

    Fix Bug

--- app.rb
+++ app.rb
@@ -6,36 +6,10 @@ require 'rest-client'
 
 class App < Sinatra::Base
 
-  # @param [string] 
-  def PostLine(message)
-    request_content = {
-        :to => [u8a142816f293877494cba3dd07f1923f],
-        :toChannel => 1383378250, # Fixed  value
-        :eventType => "138311608800106203", # Fixed value
-
-        :content => {
-            :contentType => 1,
-            :toType => 1,
-            :text => "Hello World"
-        }
-    }
-    endpoint_uri = 'https://trialbot-api.line.me/v1/events'
-    content_json = request_content.to_json
-
-    RestClient.proxy = ENV['FIXIE_URL'] if ENV['FIXIE_URL']
-    RestClient.post(endpoint_uri, content_json, {
-        'Content-Type' => 'application/json; charset=UTF-8',
-        'X-Line-ChannelID' => ENV["LINE_CHANNEL_ID"],
-        'X-Line-ChannelSecret' => ENV["LINE_CHANNEL_SECRET"],
-        'X-Line-Trusted-User-With-ACL' => ENV["LINE_CHANNEL_MID"],
-    })
-
-  end
-
-  Post '/LinePost' do
+  Get '/LinePost' do
 
     request_content = {
-        :to => ["u8a142816f293877494cba3dd07f1923f"],
+        :to => ["u8a142816f293877494cba3dd07f1923f","u8a142816f293877494cba3dd07f1923f"],
         :toChannel => 1383378250, # Fixed  value
         :eventType => "138311608800106203", # Fixed value
 
