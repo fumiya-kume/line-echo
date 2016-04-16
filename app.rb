12192c4adfe9136d718771d5adf08f3104d7b803
Ruby mine < 7dbedce569ac3038e978a9bc9b89313a1aee5edd>
2016-04-16 13:34:40 +0900

    Fix Bug

--- app.rb
+++ app.rb
@@ -6,6 +6,34 @@ require 'rest-client'
 
 class App < Sinatra::Base
 
+  get '/postline' do
+    request_content = {
+        :to => ["u8a142816f293877494cba3dd07f1923f"],
+        :toChannel => 1383378250, # Fixed  value
+        :eventType => "138311608800106203", # Fixed value
+        #オウム返しする場合
+
+        #[u8a142816f293877494cba3dd07f1923f]
+
+        content: {
+            :contentType => 1,
+            :toType => 1,
+            :text => "Hello World"
+        }
+    }
+
+    endpoint_uri = 'https://trialbot-api.line.me/v1/events'
+    content_json = request_content.to_json
+
+    RestClient.proxy = ENV['FIXIE_URL'] if ENV['FIXIE_URL']
+    RestClient.post(endpoint_uri, content_json, {
+        'Content-Type' => 'application/json; charset=UTF-8',
+        'X-Line-ChannelID' => ENV["LINE_CHANNEL_ID"],
+        'X-Line-ChannelSecret' => ENV["LINE_CHANNEL_SECRET"],
+        'X-Line-Trusted-User-With-ACL' => ENV["LINE_CHANNEL_MID"],
+    })
+  end
+
   post '/linebot/callback' do
     params = JSON.parse(request.body.read)
 
@@ -39,6 +67,40 @@ class App < Sinatra::Base
 
     "OK"
   end
+
+  # post '/linebot/callback' do
+  #   params = JSON.parse(request.body.read)
+  #
+  #   params['result'].each do |msg|
+  #     request_content = {
+  #         :to => ["u8a142816f293877494cba3dd07f1923f"],
+  #         :toChannel => 1383378250, # Fixed  value
+  #         :eventType => "138311608800106203", # Fixed value
+  #         #オウム返しする場合
+  #
+  #         #[u8a142816f293877494cba3dd07f1923f]
+  #
+  #         content: {
+  #             :contentType => 1,
+  #             :toType => 1,
+  #             :text => "Hello World"
+  #         }
+  #     }
+  #
+  #     endpoint_uri = 'https://trialbot-api.line.me/v1/events'
+  #     content_json = request_content.to_json
+  #
+  #     RestClient.proxy = ENV['FIXIE_URL'] if ENV['FIXIE_URL']
+  #     RestClient.post(endpoint_uri, content_json, {
+  #         'Content-Type' => 'application/json; charset=UTF-8',
+  #         'X-Line-ChannelID' => ENV["LINE_CHANNEL_ID"],
+  #         'X-Line-ChannelSecret' => ENV["LINE_CHANNEL_SECRET"],
+  #         'X-Line-Trusted-User-With-ACL' => ENV["LINE_CHANNEL_MID"],
+  #     })
+  #   end
+  #
+  #   "OK"
+  # end
 
   # post '/linebot/callback' do
   #   params = JSON.parse(request.body.read)
