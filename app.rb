300e65d9732bada6fb09ea6ceabd1aafefde8ca5
Ruby mine < 7dbedce569ac3038e978a9bc9b89313a1aee5edd>
2016-04-16 13:02:12 +0900

    Fix Bug

--- app.rb
+++ app.rb
@@ -6,34 +6,6 @@ require 'rest-client'
 
 class App < Sinatra::Base
 
-  Get '/LinePost' do
-
-    request_content = {
-        :to => ["u8a142816f293877494cba3dd07f1923f","u8a142816f293877494cba3dd07f1923f"],
-        :toChannel => 1383378250, # Fixed  value
-        :eventType => "138311608800106203", # Fixed value
-
-        :content => {
-            :contentType => 1,
-            :toType => 1,
-            :text => "Hello World"
-        }
-    }
-    endpoint_uri = 'https://trialbot-api.line.me/v1/events'
-    content_json = request_content.to_json
-
-    RestClient.proxy = ENV['FIXIE_URL'] if ENV['FIXIE_URL']
-    RestClient.post(endpoint_uri, content_json, {
-        'Content-Type' => 'application/json; charset=UTF-8',
-        'X-Line-ChannelID' => ENV["LINE_CHANNEL_ID"],
-        'X-Line-ChannelSecret' => ENV["LINE_CHANNEL_SECRET"],
-        'X-Line-Trusted-User-With-ACL' => ENV["LINE_CHANNEL_MID"],
-    })
-
-    'OK'
-  end
-
-
   post '/linebot/callback' do
     params = JSON.parse(request.body.read)
 
@@ -101,5 +73,6 @@ class App < Sinatra::Base
   #
   #   "OK"
   # end
+
 
 end
